38fd19f5b5270e605654b1395c29a88010347fef
Kento Shigyo <kentoglwo@gmail.com>
2020-04-29 18:11:43 +0900

    :sparkles: organize files for components

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./stylesheets/App.css";
+import logo from "../../images/logo.svg";
+
+class Header extends React.Component<Props, State> {
+  public render() {
+    <div>
+      <h1>Header</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>;
+  }
+}
+
+export default Header;

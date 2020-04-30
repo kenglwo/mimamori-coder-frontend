2e7b805980959da8ab9bb463e7b584378b1daf53
Kento Shigyo <kentoglwo@gmail.com>
2020-04-30 11:36:03 +0900

    :hammmer: reorganize components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Header from "./components/common/Header";
+import Sidebar from "./components/common/Sidebar";
 import TopPage from "./components/top/TopPage";
 import "./stylesheets/App.scss";
 
@@ -10,11 +15,23 @@ class App extends React.Component<Props, State> {
   public render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route exact={true} path={"/"} component={TopPage} />
-          </Switch>
-        </BrowserRouter>
+        <Container className="p-0" fluid>
+          <Row>
+            <Header />
+          </Row>
+          <Row>
+            <Col sm={2}>
+              <Sidebar />
+            </Col>
+            <Col sm={10}>
+              <BrowserRouter>
+                <Switch>
+                  <Route exact={true} path={"/"} component={TopPage} />
+                </Switch>
+              </BrowserRouter>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
--- src/components/top/TopPage.tsx
+++ src/components/top/TopPage.tsx
@@ -3,8 +3,6 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import Header from "../common/Header";
-import Sidebar from "../common/Sidebar";
 import Content from "./Content";
 
 interface Props {}
@@ -12,21 +10,7 @@ interface State {}
 
 class TopPage extends React.Component<Props, State> {
   public render() {
-    return (
-      <Container className="p-0" fluid>
-        <Row>
-          <Header />
-        </Row>
-        <Row>
-          <Col sm={2}>
-            <Sidebar />
-          </Col>
-          <Col sm={10}>
-            <Content />
-          </Col>
-        </Row>
-      </Container>
-    );
+    return <Content />;
   }
 }
 

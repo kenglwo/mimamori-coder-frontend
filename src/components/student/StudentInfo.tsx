5266a554d5e782588d4be75a4c18cfca6db07258
Kento Shigyo <kentoglwo@gmail.com>
2020-05-02 22:45:43 +0900

    :sparkles: add props from StudentTable via react-router-dom

--- src/components/student/StudentInfo.tsx
+++ src/components/student/StudentInfo.tsx
@@ -1,33 +1,13 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Table from "react-bootstrap/Table";
 
-interface Props {}
+interface Props {
+  student_id: string;
+  student_name: string;
+}
 interface State {}
 
 class StudentInfo extends React.Component<Props, State> {
-  // constructor(props: Props) {
-  //   super(props);
-
-  // this.state = {
-  //   student_id: student_id,
-  //   student_name: "",
-  //     // };
-  //   }
-
-  public componentDidMount() {
-    // fetch sudent data with the student_id
-    // ...
-    // const student_name = "John";
-    //
-    // this.setState((state) => {
-    //   return {
-    //     student_name: student_name,
-    //   };
-    // });
-  }
 
   public render() {
     return (
@@ -39,11 +19,11 @@ class StudentInfo extends React.Component<Props, State> {
           <tbody>
             <tr>
               <td>Student ID</td>
-              <td>185c124c</td>
+              <td>{this.props.student_id}</td>
             </tr>
             <tr>
               <td>Student Name</td>
-              <td>John Rock</td>
+              <td>{this.props.student_name}</td>
             </tr>
           </tbody>
         </Table>

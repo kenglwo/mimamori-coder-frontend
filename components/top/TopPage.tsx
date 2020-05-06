ad4c6baff668e3215312e7c64be39b681b73b826
Kento Shigyo <kentoglwo@gmail.com>
2020-05-06 17:51:12 +0900

    :zap: update components

---
+++ components/top/TopPage.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Header from "./components/common/Header";
+import Sidebar from "./components/common/Sidebar";
+
+import Overview from "./components/overview/Overview";
+import StudentView from "./components/student/StudentView";
+
+import "./stylesheets/App.scss";
+
+interface Props {}
+interface State {}
+
+class App extends React.Component<Props, State> {
+  public render() {
+    return (
+      <div className="App">
+        <Container className="p-0" fluid>
+          <Row>
+            <Header />
+          </Row>
+          <Row>
+            <Col sm={2} className="p-0">
+              <Sidebar />
+            </Col>
+            <Col sm={10} className="p-0">
+              <BrowserRouter>
+                <Switch>
+                  <Route path={"/overview"} component={Overview} />
+                  <Route
+                    path={"/student/:student_id/:student_name"}
+                    component={StudentView}
+                  />
+                </Switch>
+              </BrowserRouter>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;

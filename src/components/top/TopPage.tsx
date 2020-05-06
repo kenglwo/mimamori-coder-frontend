d2be8fff7721ab7750a99a3129b71c9325c2f4a9
Kento Shigyo <kentoglwo@gmail.com>
2020-05-06 17:48:50 +0900

    :zap: update components of top

--- src/components/top/TopPage.tsx
+++ src/components/top/TopPage.tsx
@@ -1,25 +1,14 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-
-import Toolbar from "./Toolbar";
-import StudentsTable from "./StudentsTable";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
 interface Props {}
 interface State {}
 
 class TopPage extends React.Component<Props, State> {
   public render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Toolbar />
-        </Row>
-        <Row>
-          <StudentsTable />
-        </Row>
-      </Container>
-    );
+    return <div className="text-white">Top Page</div>;
   }
 }
 

9bba3867ad2ae301a0cb1e5496210cf4be0a534e
Kento Shigyo <kentoglwo@gmail.com>
2020-05-13 21:24:22 +0900

    :recycle: rename state elements

--- src/components/student/StudentView.tsx
+++ src/components/student/StudentView.tsx
@@ -11,6 +11,8 @@ import CodePane from "./CodePane";
 import ConsolePane from "./ConsolePane";
 import PreviewPane from "./PreviewPane";
 
+import { StudentViewItem } from "../models/Types";
+
 import { commit_log_seed } from "./commit_log_seed";
 
 interface DebugMessage {
@@ -25,45 +27,27 @@ interface Commit {
   commitIndex: number;
 }
 
-interface File {
-  fileName: string;
-  updatedTime: string;
-  code: string;
-  codeStatus: "ok" | "error" | "warning";
-  errorMessages: DebugMessage[];
-  warningMessages: DebugMessage[];
-  commitIndex: number;
-  commitId: string;
-}
-
 interface CommitInfo {}
 
 interface Props
-  extends RouteComponentProps<{ student_id: string; student_name: string }> {}
-interface State {
-  student_id: string;
-  student_name: string;
-  currentCommitIndex: number;
-  commitTotalNum: number;
-  filename: string;
-  updated_time: string;
-}
+  extends RouteComponentProps<{ studentID: string; studentName: string }> {}
+interface State extends StudentViewItem {}
 
 class StudentView extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      student_id: this.props.match.params.student_id,
-      student_name: this.props.match.params.student_name,
-      currentCommitIndex: 0,
+      studentID: this.props.match.params.studentID,
+      studentName: this.props.match.params.studentName,
       commitTotalNum: 0,
-      filename: "",
-      updated_time: "",
+      currentCommitIndex: 0,
+      files: [],
     };
 
     this.showOlderCommit = this.showOlderCommit.bind(this);
     this.showNewerCommit = this.showNewerCommit.bind(this);
+    this.loadStudentItem = this.loadStudentItem.bind(this);
   }
 
   public showOlderCommit() {
@@ -88,10 +72,34 @@ class StudentView extends React.Component<Props, State> {
     });
   }
 
+  public loadStudentItem() {
+    const url = `http://localhost:3001/api/student_view?student_id=${this.state.studentID}`;
+
+    fetch(url, { mode: "cors" })
+      .then((res) => res.json())
+      .then(
+        (jsonData) => {
+          this.setState({
+            commitTotalNum: jsonData["commitTotalNum"],
+            currentCommitIndex: jsonData["commitTotalNum"],
+            files: jsonData.files,
+          });
+        },
+        (error) => {
+          console.log("Error: loadAllStudentTableItems");
+          // this.setState({
+          //   // error,
+          //   isLoaded: true
+          /// });
+        }
+      );
+  }
+
   public componentDidMount() {
-    // TODO: fetch sudent data with the student_id using API
+    // TODO: fetch sudent data with the studentID using API
 
     // 1. commitTotalNum
+    // const commitTotalNum: number = commit_log_seed.length;
     const commitTotalNum: number = commit_log_seed.length;
 
     // 2. fetch filename of the latest commit
@@ -100,14 +108,11 @@ class StudentView extends React.Component<Props, State> {
     // 3. fetch updated_time of the filename
     const updated_time = "";
 
-    this.setState((state) => {
-      return {
-        currentCommitIndex: commitTotalNum,
-        commitTotalNum: commitTotalNum,
-        filename: filename,
-        updated_time: updated_time,
-      };
-    });
+    // this.setState((state) => {
+    //   return {
+    //     files: [],
+    //   };
+    // });
   }
 
   public render() {
@@ -116,12 +121,12 @@ class StudentView extends React.Component<Props, State> {
         <Row>
           <Col md={3} className="p-0">
             <StudentInfo
-              student_id={this.state.student_id}
-              student_name={this.state.student_name}
+              studentID={this.state.studentID}
+              studentName={this.state.studentName}
             />
-            <FileListPane student_id={this.state.student_id} />
+            <FileListPane studentID={this.state.studentID} />
             <CommitLog
-              student_id={this.state.student_id}
+              studentID={this.state.studentID}
               currentCommitIndex={this.state.currentCommitIndex}
               commitTotalNum={this.state.commitTotalNum}
             />
@@ -131,7 +136,7 @@ class StudentView extends React.Component<Props, State> {
               <Row>
                 <Col md={6} className="p-0">
                   <CodePane
-                    student_id={this.state.student_id}
+                    studentID={this.state.studentID}
                     currentCommitIndex={this.state.currentCommitIndex}
                     commitTotalNum={this.state.commitTotalNum}
                     showOlderCommit={this.showOlderCommit}

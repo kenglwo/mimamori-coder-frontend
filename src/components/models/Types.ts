5d82ecf7cc311bd1ad9939e6b802ff75a2f9bab1
Kento Shigyo <kentoglwo@gmail.com>
2020-05-13 21:21:45 +0900

    :hammer: update types

--- src/components/models/Types.ts
+++ src/components/models/Types.ts
@@ -8,18 +8,32 @@ export type DisplayOption = {
     | "errors";
 };
 
-export type CodeStatus = "ok" | "warning" | "error";
+export type CodeStatus =
+  | "ok"
+  | "warning"
+  | "error"
+  | "warning-error"
+  | "unknown";
 
 export type FileInfo = {
   fileName: string;
-  lastUpdatedTime: string;
-  codeStatus: CodeStatus[];
-  warningsCount: number;
-  errorsCount: number;
+  commitIndex: number;
+  updatedTime: string;
+  codeStatus: CodeStatus;
+  warningNum: number;
+  errorNum: number;
 };
 
 export type StudentTableItem = {
   studentID: string;
   studentName: string;
   workingFiles: FileInfo[];
+};
+
+export type StudentViewItem = {
+  studentID: string;
+  studentName: string;
+  commitTotalNum: number;
+  currentCommitIndex: number;
+  files: FileInfo[];
 };

80d5720d43b26c376e825e9516f6461562427038
Kento Shigyo <kentoglwo@gmail.com>
2020-05-16 17:37:44 +0900

    :recycle: delete console.log

--- src/components/student/CodePane.tsx
+++ src/components/student/CodePane.tsx
@@ -25,15 +25,18 @@ interface Props {
   showOlderCommit: () => void;
   showNewerCommit: () => void;
 }
-interface State {
-  studentID: string;
+
+type CodeInfo = {
   fileName: string;
-  updatedTime: string;
-  // currentCommitIndex: number;
-  // commitTotalNum: number;
+  commitTime: string;
   codeString: string;
   codeStatus: string;
+};
+interface State {
+  studentID: string;
   syntaxStyle: string;
+  currentCommitIndex: number;
+  files: CodeInfo[];
 }
 
 class CodePane extends React.Component<Props, State> {
@@ -41,104 +44,109 @@ class CodePane extends React.Component<Props, State> {
     super(props);
 
     this.state = {
-      studentID: this.props.studentID,
-      fileName: "",
-      updatedTime: "",
-      // currentCommitIndex: this.props.currentCommitIndex,
-      // commitTotalNum: this.props.commitTotalNum,
-      codeString: "",
-      codeStatus: "",
       syntaxStyle: darcula,
+      studentID: this.props.studentID,
+      files: [],
+      currentCommitIndex: this.props.currentCommitIndex,
     };
+
+    this.loadCodeInfo = this.loadCodeInfo.bind(this);
   }
 
-  public componentDidMount() {
-    // TODO: fetch sudent data with the studentID
-    // 1. fetch codeString with studentID and currentCommitIndex
-    const codeString: string = `
-<html>
-	<head>
-	<meta charset="utf-8" />
-	<title>タイトル</title>
-	</head>
-	<body>
-		<h1>Hello World!</h1>
-	</body>
-</html>
-    	`;
+  public loadCodeInfo(currentCommitIndex: number) {
+    if (currentCommitIndex > 0) {
+      const url = ` http://localhost:3001/api/student_view/code?student_id=${this.state.studentID}&current_commit_index=${currentCommitIndex}`;
 
-    // 2. fetch filename
-    // 3. fetch updatedTime
-    // 4. fetch codeStatus
-
-    this.setState((state) => {
-      return {
-        codeString: codeString,
-        fileName: "hello.index",
-        updatedTime: "2020-05-02 14:50",
-        codeStatus: "ok",
-      };
-    });
+      fetch(url, { mode: "cors" })
+        .then((res) => res.json())
+        .then(
+          (jsonData) => {
+            this.setState({
+              files: jsonData,
+            });
+          },
+          (error) => {
+            console.log("Error: loadAllStudentTableItems");
+            // this.setState({
+            //   // error,
+            //   isLoaded: true
+            /// });
+          }
+        );
+    }
   }
 
-  // static getDerivedStateFromProps(nextProps: Props, prevState: State) {
-  //   return {
-  //     currentCommitIndex: nextProps.currentCommitIndex,
-  //   };
-  // }
+  public componentDidUpdate(prevProps: Props) {
+    if (this.props.currentCommitIndex !== prevProps.currentCommitIndex) {
+      this.setState({ currentCommitIndex: this.props.currentCommitIndex });
+      this.loadCodeInfo(this.props.currentCommitIndex);
+    }
+  }
 
   public render() {
-    return (
-      <div>
-        <div className="bg-info p-1 text-white font-weight-bold">Code Pane</div>
-        <div
-          className="d-flex justify-content-start pt-4 pl-2"
-          id="filename_tag"
-        >
-          <span className="badge badge-secondary">{this.state.fileName}</span>
-          {this.state.codeStatus === "ok" && (
-            <span className="badge badge-primary ml-3">OK</span>
-          )}
-          {this.state.codeStatus === "error" && (
-            <span className="badge badge-danger ml-3">Error</span>
-          )}
-          {this.state.codeStatus === "warning" && (
-            <span className="badge badge-warning ml-3">Warning</span>
-          )}
+    if (this.state.files.length === 0) {
+      return <div></div>;
+    } else {
+      const codePanes = this.state.files.map((file, i) => (
+        <div key={i}>
+          <div
+            className="d-flex justify-content-start pt-4 pl-2"
+            id="filename_tag"
+          >
+            <span className="badge badge-secondary">
+              {this.state.files[i]["fileName"]}
+            </span>
+            {this.state.files[i]["codeStatus"] === "ok" && (
+              <span className="badge badge-primary ml-3">OK</span>
+            )}
+            {this.state.files[i]["codeStatus"] === "error" && (
+              <span className="badge badge-danger ml-3">Error</span>
+            )}
+            {this.state.files[i]["codeStatus"] === "warning" && (
+              <span className="badge badge-warning ml-3">Warning</span>
+            )}
+          </div>
+          <SyntaxHighlighter
+            className="m-3"
+            language="html"
+            style={this.state.syntaxStyle}
+          >
+            {this.state.files[i]["codeString"]}
+          </SyntaxHighlighter>
         </div>
-        <SyntaxHighlighter
-          className="m-3"
-          language="html"
-          style={this.state.syntaxStyle}
-        >
-          {this.state.codeString}
-        </SyntaxHighlighter>
-
-        <div className="mb-4">
-          {this.props.currentCommitIndex > 1 && (
-            <button
-              type="button"
-              className="btn btn-outline-secondary"
-              onClick={this.props.showOlderCommit}
-            >
-              {"<"}
-            </button>
-          )}
-          <span className="badge badge-secondary ml-4 mr-4">
-            {this.state.updatedTime}
-          </span>
-          {this.props.currentCommitIndex < this.props.commitTotalNum && (
-            <button
-              type="button"
-              className="btn btn-outline-secondary"
-              onClick={this.props.showNewerCommit}
-            >
-              {">"}
-            </button>
-          )}
+      ));
+      return (
+        <div>
+          <div className="bg-info p-1 text-white font-weight-bold">
+            Code Pane
+          </div>
+          {codePanes}
+          <div className="mb-4">
+            {this.state.currentCommitIndex > 1 && (
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                onClick={this.props.showOlderCommit}
+              >
+                {"<"}
+              </button>
+            )}
+            <span className="badge badge-secondary ml-4 mr-4">
+              {this.state.files[0]["commitTime"]}
+            </span>
+            {this.state.currentCommitIndex < this.props.commitTotalNum && (
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                onClick={this.props.showNewerCommit}
+              >
+                {">"}
+              </button>
+            )}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 

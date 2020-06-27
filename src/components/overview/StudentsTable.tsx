3afdf5c385dc8fa49bee2574ef6cc4850c8088de
Kento Shigyo <kentoglwo@gmail.com>
2020-06-27 22:51:27 +0900

    :sparkles: add function to change display order

--- src/components/overview/StudentsTable.tsx
+++ src/components/overview/StudentsTable.tsx
@@ -8,10 +8,13 @@ import { DisplayOption, StudentTableItem } from "../models/Types";
 import "../../stylesheets/StudentsTable.scss";
 
 interface Props extends RouteComponentProps<{ studentID: string }> {
-  displayOption: DisplayOption;
+  displayStyle: string;
+  displayOrder: string;
 }
 interface State {
   studentsTableItems: StudentTableItem[];
+  displayStyle: string;
+  displayOrder: string;
 }
 
 class StudentsTable extends React.Component<Props, State> {
@@ -20,6 +23,8 @@ class StudentsTable extends React.Component<Props, State> {
 
     this.state = {
       studentsTableItems: [],
+      displayStyle: this.props.displayStyle,
+      displayOrder: this.props.displayOrder,
     };
 
     this.onTableRowCicked = this.onTableRowCicked.bind(this);
@@ -50,8 +55,99 @@ class StudentsTable extends React.Component<Props, State> {
     }
   }
 
+  public componentDidUpdate(prevProps: Props) {
+    if (this.props.displayOrder !== prevProps.displayOrder) {
+      this.setState({ displayOrder: this.props.displayOrder });
+    }
+  }
+
+  public orderByStudentID(a: StudentTableItem, b: StudentTableItem) {
+    const studentIDA = a.studentID.toUpperCase();
+    const studentIDB = b.studentID.toUpperCase();
+
+    let comparison = 0;
+    if (studentIDA > studentIDB) {
+      comparison = 1;
+    } else if (studentIDA < studentIDB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+  public orderByCommitNumAsc(a: StudentTableItem, b: StudentTableItem) {
+    const studentA = Number(a.workingFiles[0].commitIndex);
+    const studentB = Number(b.workingFiles[0].commitIndex);
+
+    let comparison = 0;
+    if (studentA > studentB) {
+      comparison = 1;
+    } else if (studentA < studentB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+  public orderByCommitNumDesc(a: StudentTableItem, b: StudentTableItem) {
+    const studentA = Number(a.workingFiles[0].commitIndex);
+    const studentB = Number(b.workingFiles[0].commitIndex);
+
+    let comparison = 0;
+    if (studentA < studentB) {
+      comparison = 1;
+    } else if (studentA > studentB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+  public orderByLastUpdatedTimeAsc(a: StudentTableItem, b: StudentTableItem) {
+    const studentA = a.workingFiles[0].updatedTime;
+    const studentB = b.workingFiles[0].updatedTime;
+
+    let comparison = 0;
+    if (studentA > studentB) {
+      comparison = 1;
+    } else if (studentA < studentB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+  public orderByLastUpdatedTimeDesc(a: StudentTableItem, b: StudentTableItem) {
+    const studentA = a.workingFiles[0].updatedTime;
+    const studentB = b.workingFiles[0].updatedTime;
+
+    let comparison = 0;
+    if (studentA < studentB) {
+      comparison = 1;
+    } else if (studentA > studentB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+
   public render() {
-    const table_rows = this.state.studentsTableItems.map((item, i) => {
+    let studentsTableItems = this.state.studentsTableItems.filter(
+      (item) => item.workingFiles[0].fileName !== "unknown"
+    );
+    studentsTableItems =
+      this.state.displayOrder === "studentID"
+        ? studentsTableItems.sort(this.orderByStudentID)
+        : studentsTableItems;
+    studentsTableItems =
+      this.state.displayOrder === "commits_asc"
+        ? studentsTableItems.sort(this.orderByCommitNumAsc)
+        : studentsTableItems;
+    studentsTableItems =
+      this.state.displayOrder === "commits_desc"
+        ? studentsTableItems.sort(this.orderByCommitNumDesc)
+        : studentsTableItems;
+    studentsTableItems =
+      this.state.displayOrder === "updated_time_asc"
+        ? studentsTableItems.sort(this.orderByLastUpdatedTimeAsc)
+        : studentsTableItems;
+    studentsTableItems =
+      this.state.displayOrder === "updated_time_desc"
+        ? studentsTableItems.sort(this.orderByLastUpdatedTimeDesc)
+        : studentsTableItems;
+
+    const table_rows = studentsTableItems.map((item, i) => {
       const checkCodeStatus = (codeStatus: string): string => {
         if (codeStatus === "error") {
           return "code_error";
